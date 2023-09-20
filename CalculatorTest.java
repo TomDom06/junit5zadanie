f06f76f7d5e3caa4b8f984e837bb8a36b7ebf1a2
TomDom06 <128038053+TomDom06@users.noreply.github.com>
2023-09-20 11:42:08 +0200

    Add files via upload

---
+++ CalculatorTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(15, calculator.multiply(5, 3));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2.0, calculator.divide(6, 3));
+    }
+
+    @Test
+    void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(6, 0));
+    }
+}
